cc6c97401ace7e9996e29b9f5b3881d1fcc7b042
MS <72651078+maddydevgits@users.noreply.github.com>
2025-07-23 12:11:35 +0530

    updating code

--- main.ino
+++ main.ino
@@ -3,7 +3,7 @@
 #include <IRutils.h>
 
 const uint16_t RECV_PIN = 23;  // TSOP1738 OUT to GPIO23
-const int LED_PIN = 2;         // Built-in LED or external
+const int RELAY = 22;         // Relay IN to GPIO22
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -11,8 +11,7 @@ decode_results results;
 void setup() {
   Serial.begin(115200);
   irrecv.enableIRIn();
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  pinMode(RELAY, OUTPUT);
   Serial.println("IR Receiver Ready. Waiting for input...");
 }
 
@@ -24,12 +23,12 @@ void loop() {
 
     // Replace with your own remote codes
     if (code == 0xFFA25D) {         // POWER button (example)
-      digitalWrite(LED_PIN, 0);  // LED ON
-      Serial.println("LED ON");
+      digitalWrite(RELAY, 0);  // LIGHT ON
+      Serial.println("LIGHT ON");
     }
     else if (code == 0xFF629D) {    // VOL+ button (example)
-      digitalWrite(LED_PIN, 1);   // LED OFF
-      Serial.println("LED OFF");
+      digitalWrite(RELAY, 1);   // LIGHT OFF
+      Serial.println("LIGHT OFF");
     }
 
     irrecv.resume();  // Receive next
